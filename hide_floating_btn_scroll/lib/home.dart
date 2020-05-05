4097a88b46ea2535bf4d9eb7b1cb01f35ca02a3d
Nuwantha Fernando <wnsfernando95@gmail.com>
2020-05-05 21:41:10 +0530

    Project finalized

---
+++ hide_floating_btn_scroll/lib/home.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  ScrollController _scrollController =
+      new ScrollController(); // set controller on scrolling
+  bool isScrollingDown = false;
+  bool _show = true;
+  List<String> items = [];
+
+  @override
+  void initState() {
+    super.initState();
+    addItemsToTheList();
+    handleScroll();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(() {});
+    super.dispose();
+  }
+
+  void showFloationButton() {
+    setState(() {
+      _show = true;
+    });
+  }
+
+  void hideFloationButton() {
+    setState(() {
+      _show = false;
+    });
+  }
+
+  void addItemsToTheList() {
+    for (int count = 1; count <= 100; count++) {
+      items.add("Book " + count.toString());
+    }
+  }
+
+  void handleScroll() async {
+    _scrollController.addListener(() {
+      if (_scrollController.position.userScrollDirection ==
+          ScrollDirection.reverse) {
+        if (!isScrollingDown) {
+          isScrollingDown = true;
+          hideFloationButton();
+        }
+      }
+      if (_scrollController.position.userScrollDirection ==
+          ScrollDirection.forward) {
+        if (isScrollingDown) {
+          isScrollingDown = false;
+          showFloationButton();
+        }
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Tutorial App"),
+        ),
+        body: Container(
+          child: new ListView.builder(
+              controller: _scrollController,
+              itemCount: items.length,
+              itemBuilder: (BuildContext ctxt, int index) {
+                return new Card(
+                    child: ListTile(
+                  title: Text(items[index]),
+                ));
+              }),
+        ),
+        floatingActionButton: Visibility(
+          visible: _show,
+            child: FloatingActionButton(
+                child: Icon(Icons.add), onPressed: () {})));
+  }
+}
